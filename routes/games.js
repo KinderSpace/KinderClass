bc7ebdbeb7cb293b6041f682563977f530546caa
Manu <manueltoscano91@hotmail.com>
2020-05-14 11:19:30 +0200

    seeds and first game route

---
+++ routes/games.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const Card = require("../models/Card");
+
+router.get("/tutti-frutti", (req, res, next) => {
+  Card.find()
+    .then((cardsFound) => {
+      console.log("cards found:", cardsFound);
+      res.status(200).json({ cards });
+    })
+    .catch((err) => {
+      res.json(err, { message: "Error while finding cards" });
+    });
+});
+
+module.exports = router;
