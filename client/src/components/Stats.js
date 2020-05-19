95f96c3b8ae75bc6dea28c2c10078110adc44b08
Manu <manueltoscano91@hotmail.com>
2020-05-19 12:45:07 +0200

    finished graph filters

--- client/src/components/Stats.js
+++ client/src/components/Stats.js
@@ -5,81 +5,100 @@ import Search from "./Search";
 import { Bar } from "react-chartjs-2";
 
 let timer;
-const data = {
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
-  datasets: [
-    {
-      label: "Scores",
-      backgroundColor: "rgba(255,99,132,0.2)",
-      borderColor: "rgba(255,99,132,1)",
-      borderWidth: 1,
-      hoverBackgroundColor: "rgba(255,99,132,0.4)",
-      hoverBorderColor: "rgba(255,99,132,1)",
-      data: [65, 59, 80, 81, 56, 55, 40],
-    },
-  ],
-};
 
 export default class Stats extends Component {
   state = {
-    data: data,
     query: "",
     game: "",
+    allData: "",
+    category: "",
+    date: 5184000000,
   };
 
   componentDidMount = () => {
-    this.getData(this.state.query, this.state.game);
+    this.getAllData();
   };
 
-  getData = (query, game) => {
-    axios.get(`/api/stats?query=${query}&game=${game}`).then((gotUsers) => {
-      //Create array of users
-      const userArray = gotUsers.data.users.map((el) => {
-        return el.username;
-      });
-      //Update data
-      data.labels = userArray;
-      //Get array of average scores
-      //Access each users matches
-      const scoreArray = gotUsers.data.users.map((user) => {
-        return (
-          //Return average of all scores
-          user.matches.reduce((acc, curVal) => {
-            return acc + curVal.score;
-          }, 0) / user.matches.length || 0
-        );
-      });
-      //Update scores
-      data.datasets[0].data = scoreArray;
+  getAllData = () => {
+    axios.get("/api/stats").then((gotUsers) => {
       this.setState({
-        data: data,
+        allData: gotUsers.data.users.map((user) => {
+          user.originalMatches = user.matches.slice(0);
+          return user;
+        }),
       });
     });
   };
 
+  filteredData = (allData) => {
+    const dataSet = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: "Scores",
+          backgroundColor: "rgba(255,99,132,0.2)",
+          borderColor: "rgba(255,99,132,1)",
+          borderWidth: 1,
+          hoverBackgroundColor: "rgba(255,99,132,0.4)",
+          hoverBorderColor: "rgba(255,99,132,1)",
+          data: [65, 59, 80, 81, 56, 55, 40],
+        },
+      ],
+    };
+    //Create array of users
+    const userArray = allData.map((el) => {
+      return el.username;
+    });
+    //Update data
+    dataSet.labels = userArray;
+    //Get array of average scores
+    //Access each users matches
+    const scoreArray = allData.map((user) => {
+      return (
+        //Return average of all scores
+        user.matches.reduce((acc, curVal) => {
+          return acc + curVal.score;
+        }, 0) / user.matches.length || 0
+      );
+    });
+    //Update scores
+    dataSet.datasets[0].data = scoreArray;
+    return dataSet;
+  };
+
   setQuery = (query) => {
-    //Frenar si hay algun timeout
-    clearTimeout(timer);
     this.setState({ query: query });
-    //Hacer un nuevo timeout y si se cumple hacer lo siguiente =>
-    timer = setTimeout(() => {
-      this.getData(this.state.query, this.state.game);
-    }, 1000);
   };
 
   handleFilters = (e) => {
     const { name, value } = e.target;
-    this.setState(
-      {
-        [name]: value,
-      },
-      () => {
-        this.getData(this.state.query, this.state.game);
-      }
-    );
+    this.setState({
+      [name]: value,
+    });
   };
 
   render() {
+    if (!this.state.allData) {
+      return <div>Loading...</div>;
+    }
+    const filteredKids = this.state.allData
+      .filter((user) => {
+        return (
+          user.username.includes(this.state.query) && user.role !== "teacher"
+        );
+      })
+      .map((kid) => {
+        kid.matches = kid.originalMatches.filter((match) => {
+          return (
+            match.game.includes(this.state.game) &&
+            match.category.includes(this.state.category) &&
+            new Date(match.date_played).getTime() >=
+              new Date().getTime() - this.state.date
+          );
+        });
+        return kid;
+      });
+    const newDataSet = this.filteredData(filteredKids);
     return (
       <div>
         <h1>Kids average score on the educational games</h1>
@@ -89,7 +108,7 @@ export default class Stats extends Component {
           handleFilters={this.handleFilters}
         />
         <Bar
-          data={this.state.data}
+          data={newDataSet}
           width={100}
           height={300}
           options={{
