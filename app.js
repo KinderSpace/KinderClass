03626fa52e1f0c55227cb03984d956f835f40b2f
Manu <manueltoscano91@hotmail.com>
2020-05-20 11:40:07 +0200

    built for deployment

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use(
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
@@ -82,6 +82,11 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/games", require("./routes/games"));
 app.use("/api/stats", require("./routes/stats"));
 app.use("/api/cloudinary", require("./routes/cloudinary"));
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
 module.exports = app;
 
